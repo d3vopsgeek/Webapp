27b89b866152906ead464a8aa8f7068b32b70cde
d3vopsgeek <41163727+d3vopsgeek@users.noreply.github.com>
2019-10-01 10:56:44 -0500

    Adding signup page

--- index.py
+++ index.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, session, make_response
 import os
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:123456@localhost:5432/isac"
+Bootstrap(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 
 @app.route('/')
@@ -14,6 +34,14 @@ def home():
         return 'logged as ' + username + '<br>' + "<b><a href = '/logout'>click here to log out</a></b>"
     else:
         return render_template('home.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return '<h2>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h2>'
+    return render_template('signup.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
